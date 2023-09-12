343b85eeed4f4c259dfec8b867cd98f560ef0b68
이철희 <leechulhee95@gmail.com>
2023-09-12 09:24:11 +0900

    key position refactor and add new combo

---
+++ config/totem_key_position.h
@@ -0,0 +1,61 @@
+#pragma once
+
+// Row 1
+#define L1_1 0
+#define L1_2 1
+#define L1_3 2
+#define L1_4 3
+#define L1_5 4
+#define R1_5 5
+#define R1_4 6
+#define R1_3 7
+#define R1_2 8
+#define R1_1 9
+
+// Row 2
+#define L2_1 10
+#define L2_2 11
+#define L2_3 12
+#define L2_4 13
+#define L2_5 14
+#define R2_5 15
+#define R2_4 16
+#define R2_3 17
+#define R2_2 18
+#define R2_1 19
+
+// Row 3
+#define L3_1 20
+#define L3_2 21
+#define L3_3 22
+#define L3_4 23
+#define L3_5 24
+#define R3_5 25
+#define R3_4 26
+#define R3_3 27
+#define R3_2 28
+#define R3_1 29
+
+// Thumb Row
+#define LT_1 32
+#define LT_2 33
+#define LT_3 34
+#define RT_3 35
+#define RT_2 36
+#define RT_1 37
+
+// Addon
+#define LA_1 30
+#define RA_1 39
+
+// Collection
+#define L1_X L1_1 L1_2 L1_3 L1_4 L1_5
+#define L2_X L2_1 L2_2 L2_3 L2_4 L2_5
+#define L3_X L3_1 L3_2 L3_3 L3_4 L3_5
+#define R1_X R1_1 R1_2 R1_3 R1_4 R1_5
+#define R2_X R2_1 R2_2 R2_3 R2_4 R2_5
+#define R3_X R3_1 R3_2 R3_3 R3_4 R3_5
+#define LT_X LT_1 LT_2 LT_3
+#define RT_X RT_1 RT_2 RT_3
+#define LA_X LA_1
+#define RA_X RA_1
