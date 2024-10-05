55b8af6afb2cac36703e284a07b409b347ff8882
ironhee <leechulhee95@gmail.com>
2024-10-05 18:45:02 +0900

    keyposition name change

--- config/totem_key_position.h
+++ config/totem_key_position.h
@@ -1,61 +1,56 @@
+/*
+    ╭─────────────────────╮ ╭─────────────────────╮
+    │ LT4 LT3 LT2 LT1 LT0 │ │ RT0 RT1 RT2 RT3 RT4 │
+╭───╯ LM4 LM3 LM2 LM1 LM0 │ │ RM0 RM1 RM2 RM3 RM4 ╰───╮
+│ LA1 LB4 LB3 LB2 LB1 LB0 │ │ RB0 RB1 RB2 RB3 RB4 RA1 │
+╰───────────╮ LH2 LH1 LH0 │ │ RH0 RH1 RH2 ╭───────────╯
+            ╰─────────────╯ ╰─────────────╯ 
+*/
 #pragma once
 
-// Row 1
-#define L1_1 0
-#define L1_2 1
-#define L1_3 2
-#define L1_4 3
-#define L1_5 4
-#define R1_5 5
-#define R1_4 6
-#define R1_3 7
-#define R1_2 8
-#define R1_1 9
+#define LT1 0
+#define LT2 1
+#define LT3 2
+#define LT4 3
+#define LT5 4
+#define RT5 5
+#define RT4 6
+#define RT3 7
+#define RT2 8
+#define RT1 9
 
-// Row 2
-#define L2_1 10
-#define L2_2 11
-#define L2_3 12
-#define L2_4 13
-#define L2_5 14
-#define R2_5 15
-#define R2_4 16
-#define R2_3 17
-#define R2_2 18
-#define R2_1 19
+#define LM1 10
+#define LM2 11
+#define LM3 12
+#define LM4 13
+#define LM5 14
+#define RM5 15
+#define RM4 16
+#define RM3 17
+#define RM2 18
+#define RM1 19
 
-// Row 3
-#define L3_1 20
-#define L3_2 21
-#define L3_3 22
-#define L3_4 23
-#define L3_5 24
-#define R3_5 25
-#define R3_4 26
-#define R3_3 27
-#define R3_2 28
-#define R3_1 29
+#define LB1 20
+#define LB2 21
+#define LB3 22
+#define LB4 23
+#define LB5 24
+#define RB5 25
+#define RB4 26
+#define RB3 27
+#define RB2 28
+#define RB1 29
 
-// Thumb Row
-#define LT_1 32
-#define LT_2 33
-#define LT_3 34
-#define RT_3 35
-#define RT_2 36
-#define RT_1 37
+#define LH1 32
+#define LH2 33
+#define LH3 34
+#define RH3 35
+#define RH2 36
+#define RH1 37
 
-// Addon
-#define LA_1 30
-#define RA_1 39
+#define LA1 30
+#define RA1 39
 
-// Collection
-#define L1_X L1_1 L1_2 L1_3 L1_4 L1_5
-#define L2_X L2_1 L2_2 L2_3 L2_4 L2_5
-#define L3_X L3_1 L3_2 L3_3 L3_4 L3_5
-#define R1_X R1_1 R1_2 R1_3 R1_4 R1_5
-#define R2_X R2_1 R2_2 R2_3 R2_4 R2_5
-#define R3_X R3_1 R3_2 R3_3 R3_4 R3_5
-#define LT_X LT_1 LT_2 LT_3
-#define RT_X RT_1 RT_2 RT_3
-#define LA_X LA_1
-#define RA_X RA_1
+#define KEYS_LEFT LT1 LT2 LT3 LT4 LT5 LM1 LM2 LM3 LM4 LM5 LB1 LB2 LB3 LB4 LB5
+#define KEYS_RIGHT RT1 RT2 RT3 RT4 RT5 RM1 RM2 RM3 RM4 RM5 RB1 RB2 RB3 RB4 RB5
+#define KEYS_ETC LH1 LH2 LH3 RH1 RH2 RH3 LA_1 RA_1
