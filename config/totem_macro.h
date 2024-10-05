65f185044f14fffe3408bb91976094d50cf63161
ironhee <leechulhee95@gmail.com>
2024-10-05 15:07:06 +0900

    update

--- config/totem_macro.h
+++ config/totem_macro.h
@@ -1,58 +1,79 @@
 #pragma once
 
-#define WRAP_QUOTE(X) #X
+// Preprocessor mechanism to overload macros, cf. https://stackoverflow.com/a/27051616/6114651
+#define VARGS_(_10, _9, _8, _7, _6, _5, _4, _3, _2, _1, N, ...) N
+#define VARGS(...) VARGS_(__VA_ARGS__, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+#define CONCAT_(a, b) a##b
+#define CONCAT(a, b) CONCAT_(a, b)
+#define ZMK_HELPER_STRINGIFY(x) #x
 
-// clang-format off
-#define IRONHEE_MACRO(NAME, ...) \
-  NAME: NAME { \
-    label = WRAP_QUOTE(MACRO_ ## NAME); \
-    compatible = "zmk,behavior-macro"; \
-    #binding-cells = <0>; \
-    wait-ms = <1>; \
-    tap-ms = <1>; \
-    bindings = __VA_ARGS__; \
+/*=====================*/
+/* Behavior            */
+/*=====================*/
+#define ZMK_BEHAVIOR_CORE_caps_word       compatible = "zmk,behavior-caps-word";       #binding-cells = <0>
+#define ZMK_BEHAVIOR_CORE_hold_tap        compatible = "zmk,behavior-hold-tap";        #binding-cells = <2>
+#define ZMK_BEHAVIOR_CORE_key_repeat      compatible = "zmk,behavior-key-repeat";      #binding-cells = <0>
+#define ZMK_BEHAVIOR_CORE_mod_morph       compatible = "zmk,behavior-mod-morph";       #binding-cells = <0>
+#define ZMK_BEHAVIOR_CORE_sticky_key      compatible = "zmk,behavior-sticky-key";      #binding-cells = <1>
+#define ZMK_BEHAVIOR_CORE_tap_dance       compatible = "zmk,behavior-tap-dance";       #binding-cells = <0>
+#define ZMK_BEHAVIOR(name, type, ...) \
+  / { \
+    behaviors { \
+      name: name { \
+        ZMK_BEHAVIOR_CORE_ ## type; \
+        __VA_ARGS__ \
+      }; \
+    }; \
   };
-#define IRONHEE_HOME_ROW(NAME, HOLD, TAP, HOLD_TRIGGER_KEY_POSITIONS) \
-  NAME: NAME { \
-    compatible = "zmk,behavior-hold-tap"; \
-    label = WRAP_QUOTE(HOLD_TAP_ ## NAME); \
-    #binding-cells = <2>; \
-    flavor = "balanced"; \
-    tapping-term-ms = <280>; \
-    quick-tap-ms = <175>; \
-    require-prior-idle-ms = <150>; \
-    bindings = <HOLD>, <TAP>; \
-    hold-trigger-key-positions = <HOLD_TRIGGER_KEY_POSITIONS>; \
-    hold-trigger-on-release; \
+#define ZMK_CAPS_WORD(name, ...) ZMK_BEHAVIOR(name, caps_word, __VA_ARGS__)
+#define ZMK_HOLD_TAP(name, ...) ZMK_BEHAVIOR(name, hold_tap, __VA_ARGS__)
+#define ZMK_KEY_REPEAT(name, ...) ZMK_BEHAVIOR(name, key_repeat, __VA_ARGS__)
+#define ZMK_MOD_MORPH(name, ...) ZMK_BEHAVIOR(name, mod_morph, __VA_ARGS__)
+#define ZMK_STICKY_KEY(name, ...) ZMK_BEHAVIOR(name, sticky_key, __VA_ARGS__)
+#define ZMK_TAP_DANCE(name, ...) ZMK_BEHAVIOR(name, tap_dance, __VA_ARGS__)
+
+/*=====================*/
+/* Layer               */
+/*=====================*/
+#define ZMK_LAYER(name, layout) \
+  / { \
+    keymap { \
+      compatible = "zmk,keymap"; \
+      layer_ ## name { \
+        display-name = ZMK_HELPER_STRINGIFY(name); \
+        bindings = <layout>; \
+      }; \
+    }; \
   };
-#define IRONHEE_THUM_ROW(NAME, HOLD, TAP) \
-  NAME: NAME { \
-    compatible = "zmk,behavior-hold-tap"; \
-    label = WRAP_QUOTE(HOLD_TAP_ ## NAME); \
-    #binding-cells = <2>; \
-    flavor = "hold-preferred"; \
-    tapping-term-ms = <280>; \
-    quick-tap-ms = <175>; \
-    bindings = <HOLD>, <TAP>; \
+
+/*=====================*/
+/* Combo               */
+/*=====================*/
+#define ALL 0xff
+#if !defined COMBO_TERM
+  #define COMBO_TERM 30
+#endif
+
+#define ZMK_COMBO(...) CONCAT(ZMK_COMBO_, VARGS(__VA_ARGS__))(__VA_ARGS__)
+#define ZMK_COMBO_3(name, combo_bindings, keypos) \
+  ZMK_COMBO_4(name, combo_bindings, keypos, ALL)
+#define ZMK_COMBO_4(name, combo_bindings, keypos, combo_layers) \
+  ZMK_COMBO_5(name, combo_bindings, keypos, combo_layers, COMBO_TERM)
+#define ZMK_COMBO_5(name, combo_bindings, keypos, combo_layers, combo_timeout) \
+  ZMK_COMBO_6(name, combo_bindings, keypos, combo_layers, combo_timeout, 0)
+#define ZMK_COMBO_6(name, combo_bindings, keypos, combo_layers, combo_timeout, combo_idle) \
+  ZMK_COMBO_7(name, combo_bindings, keypos, combo_layers, combo_timeout, combo_idle, )
+#define ZMK_COMBO_7(name, combo_bindings, keypos, combo_layers, combo_timeout, combo_idle, combo_vaargs) \
+  / { \
+    combos { \
+      compatible = "zmk,combos"; \
+      combo_ ## name { \
+        timeout-ms = <combo_timeout>; \
+        bindings = <combo_bindings>; \
+        key-positions = <keypos>; \
+        layers = <combo_layers>; \
+        require-prior-idle-ms = <combo_idle>; \
+        combo_vaargs \
+      }; \
+    }; \
   };
-#define IRONHEE_MORP_SHIFT(NAME, BINDING_BASE, BINDING_MORP) \
-  NAME: NAME { \
-    compatible = "zmk,behavior-mod-morph"; \
-    label = WRAP_QUOTE(MORP_SHIFT_ ## NAME); \
-    #binding-cells = <0>; \
-    bindings = <BINDING_BASE>, <BINDING_MORP>; \
-    mods = <(MOD_LSFT|MOD_RSFT)>; \
-  };
-#define IRONHEE_COMBO_HORI(NAME, KEY_POSITIONS, BINDINGS) \
-  combo_ ## NAME { \
-    timeout-ms = <32>; \
-    key-positions = <KEY_POSITIONS>; \
-    bindings = <BINDINGS>; \
-  };
-#define IRONHEE_COMBO_VERT(NAME, KEY_POSITIONS, BINDINGS) \
-  combo_ ## NAME { \
-    timeout-ms = <32>; \
-    key-positions = <KEY_POSITIONS>; \
-    bindings = <BINDINGS>; \
-  };
-// clang-format on
